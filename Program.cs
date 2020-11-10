895504fc02895440b8dfe9111535efac22fcf3a8
MarshallTheHollow <kuajtaa@mail.ru>
2020-11-10 19:33:11 +0600

    +add Lavrov command

--- Program.cs
+++ Program.cs
@@ -8,33 +8,24 @@ namespace Калькуратор_отжиманий
     [Serializable]
     class Program
     {
-        static int p = 0;
-        static int otg = 0;
+        static int p;
+        static int otg;
         static string number;
+        static int LCount = 0;
         static BinaryFormatter binFormat = new BinaryFormatter();
         static void Main(string[] args)
         {
-            using (FileStream fStream = new FileStream("user.dat", FileMode.OpenOrCreate))
-            {
-                otg = (int)binFormat.Deserialize(fStream);
-            }
+            DataCheck();
             Console.WriteLine("Добро пожаловать!");
         Start:          
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine($"Ваши отжимания: {otg}\n");
+            Console.WriteLine($"Ваши отжимания: {otg}\n");           
+            Console.WriteLine($"Лавров съел уже {LCount} барбарисок\n");
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Существующие команды:\nДобавить - добавляет ваши отжимания\nОчистить - очищает ваши отжимания\nEsc - выход");
-            ConsoleKey Keyy = Console.ReadKey().Key;
-            if (Keyy == ConsoleKey.Escape)
-            {
-                Goodbye();
-            }
-            else
-            {
-                string selection = Console.ReadLine();               
-                Sich(selection);
-                goto Start;               
-            }
+            Console.WriteLine("Существующие команды:\nДобавить - добавляет ваши отжимания\nОчистить - очищает ваши отжимания\nЛавров - Владислав Васильевич съедает еще одну барбариску\nВыход - выход");
+            string selection = Console.ReadLine();               
+            Sich(selection);
+            goto Start;               
         }
         static int OtgCheck()
         {
@@ -61,7 +52,7 @@ namespace Калькуратор_отжиманий
         {
             switch (selection)
             {
-                case "обавить":
+                case "Добавить":
                     Console.WriteLine("Введите ваш топ");
                     try
                     {
@@ -94,10 +85,10 @@ namespace Калькуратор_отжиманий
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("ВВЕДЕН НЕВЕРНЫЙ ФОРМАТ.\n");
                         Console.ForegroundColor = ConsoleColor.White;
-                        goto case "обавить";
+                        goto case "Добавить";
                     }
                     return;
-                case "чистить":
+                case "Очистить":
                     Console.Clear();
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Отжимания очищены\n");
@@ -110,12 +101,40 @@ namespace Калькуратор_отжиманий
                         binFormat.Serialize(fStream, otg);
                     }
                     return;
+                case "Лавров":
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Принято!\n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    LCount ++;
+                    OtgCheck();
+                    using (Stream fStream2 = new FileStream("user2.dat",
+                        FileMode.Create))
+                    {
+                        binFormat.Serialize(fStream2, LCount);
+                    }
+                    return;
+                case "Выход":
+                    Console.Clear();
+                    Goodbye();
+                    return;
                 default:
                     Console.Clear();
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Вы нажали неизвестную команду\n");
                     Console.ForegroundColor = ConsoleColor.White;
                     return;
+            }
+        }
+        static void DataCheck()
+        {
+            using (FileStream fStream = new FileStream("user.dat", FileMode.OpenOrCreate))
+            {
+                otg = (int)binFormat.Deserialize(fStream);
+            }
+            using (FileStream fStream2 = new FileStream("user2.dat", FileMode.OpenOrCreate))
+            {
+                LCount = (int)binFormat.Deserialize(fStream2);
             }
         }
     }
